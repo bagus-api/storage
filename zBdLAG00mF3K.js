354df5ee8abc4a427f6fadd34daf3dd0ab7239f4
bagus-api <pribvercel@gmail.com>
2026-06-15 18:34:43 +0700

    Upload zBdLAG00mF3K.js

---
+++ zBdLAG00mF3K.js
@@ -0,0 +1,18 @@
+window.DAFTAR_SOAL = {
+    "@zzz": "credit",
+    "PSAT_PENDIDIKAN_PANCASILA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/0GS7fPtXaWCE.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_BIOLOGI_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/iAXNjDRc3l4s.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_SOSIOLOGI_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/gsndcRRmS8vu.js\",\"size\":\"0.04 MB\"}",
+    "PSAT_EKONOMI_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/4rZfsv50Mkrg.js\",\"size\":\"0.04 MB\"}",
+    "PSAT_MATEMATIKA_X": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/rwzqibWWKK2z.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_MATEMATIKA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/pZfwZa2LgPU2.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_FISIKA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/4cCwOayIVGCO.js\",\"size\":\"0.04 MB\"}",
+    "PSAT_SENI_BUDAYA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/G5bfVUPjs2Ym.js\",\"size\":\"0.02 MB\"}",
+    "PSAT_KIMIA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/QP9EQnoZouPE.js\",\"size\":\"0.02 MB\"}",
+    "PSAT_BAHASA_SUNDA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/LUKWjwO9kdqd.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_BAHASA_INGGRIS_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/TBGJ9g1KLIo3.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_BAHASA_INGGRIS_TL_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/aDABMaTI9f6L.js\",\"size\":\"0.05 MB\"}",
+    "PSAT_BAHASA_INDONESIA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/xCkjPWG2sqdO.js\",\"size\":\"0.07 MB\"}",
+    "PSAT_SEJARAH_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/xqq68hGBPddB.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_MATEMATIKA_TL_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/RtCYbRgP4paK.js\",\"size\":\"0.04 MB\"}"
+};
