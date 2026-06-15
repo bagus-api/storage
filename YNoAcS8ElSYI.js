a0b11431197bbc1e35befeeedf49cbc21bde7c38
bagus-api <pribvercel@gmail.com>
2026-06-15 18:07:01 +0700

    Upload YNoAcS8ElSYI.js

---
+++ YNoAcS8ElSYI.js
@@ -0,0 +1,14 @@
+window.DAFTAR_SOAL = {
+    "@zzz": "credit",
+    "PSAT_MATEMATIKA_X": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/mtOQ5vHHEyLr.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_MATEMATIKA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/Vyn1xkKOZh3K.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_FISIKA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/0ecS9uRKjSuN.js\",\"size\":\"0.04 MB\"}",
+    "PSAT_SENI_BUDAYA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/p7X8OITQ7OPB.js\",\"size\":\"0.02 MB\"}",
+    "PSAT_KIMIA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/nMkc14alf9YI.js\",\"size\":\"0.02 MB\"}",
+    "PSAT_BAHASA_SUNDA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/Wk7rFBM14lzp.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_BAHASA_INGGRIS_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/r0Fy8okcuOWJ.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_BAHASA_INGGRIS_TL_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/ZX6kytOT4hSW.js\",\"size\":\"0.05 MB\"}",
+    "PSAT_BAHASA_INDONESIA_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/PiwuJd7E0W5x.js\",\"size\":\"0.07 MB\"}",
+    "PSAT_SEJARAH_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/DvPgTjDwolxr.js\",\"size\":\"0.03 MB\"}",
+    "PSAT_MATEMATIKA_TL_XI": "{\"success\":true,\"url\":\"https://www.gobox.my.id/file/EtSt3k0lz8aR.js\",\"size\":\"0.04 MB\"}"
+};
